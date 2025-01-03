95b8d3b28bdef4db9040a94cc695c983084f98e4
DoomWave <acunanajair9@gmail.com>
2025-01-03 14:59:55 -0800

    a little progress

--- chatBot.py
+++ chatBot.py
@@ -1,2 +1,10 @@
+
 def chatBot ():
     print("Hello there!")
+
+    while True: 
+        user_input = input("You: ").lower()
+
+        if user_input == 'bye':
+            print("Chatbot: See ya!")
+            break
